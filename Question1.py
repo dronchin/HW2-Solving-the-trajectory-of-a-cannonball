e453aaba3d51d6d6e31118a7495b9ef2a76067c9
dronchin <35201586+dronchin@users.noreply.github.com>
2018-02-09 19:55:19 -0500

    Finished Question 1

--- Question1.py
+++ Question1.py
@@ -1,4 +1,50 @@
 import numpy as np
 import matplotlib.pyplot as plt
+#Euler method function
+def Euler(x, dx, dt):
+    return x + dt*dx
+percentError = 10
 
-print(np.pi)
+#starting dt
+dt_change = 0.01
+dt = 5 + dt_change
+while percentError > 1: #loop until wanted %error
+    #reset all intial variables
+    dt -= dt_change #shanges dt
+    x = 0
+    y = 0
+    v = 100
+    ax = 0
+    ay = -9.8
+    th = 40/180*np.pi
+    vx = v*np.cos(th)
+    vy = v*np.sin(th)
+    endx = v**2*np.sin(2*th)/abs(ay)
+    endy = 0
+    steps = 0
+    #saves coordinates for graphing
+    xes = [0]
+    yes = [0]
+    while y >= 0:
+        steps += 1 #saves number of steps
+        #updates position
+        x = Euler(x, vx, dt)
+        y = Euler(y, vy, dt)
+        #updates velocity
+        vx = Euler(vx, ax, dt)
+        vy = Euler(vy, ay, dt)
+        xes.append(x)
+        yes.append(y)
+
+    #distance calculated using pythagorean theorem
+    dist = np.sqrt((x)**2+(y)**2)
+    percentError = (abs(dist-endx)/endx)*100
+
+
+print("ending x value: ", endx)
+print("Actual x value: ", dist)
+print("step size: ", dt)
+print("number of steps: ", steps)
+print("percent error of distance: ", percentError)
+plt.plot(xes,yes)
+plt.show()
